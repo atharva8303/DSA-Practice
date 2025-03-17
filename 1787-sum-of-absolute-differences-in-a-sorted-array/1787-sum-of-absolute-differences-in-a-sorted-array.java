9d825d45720879ff4d0dd8a49067d88d109f67cf
atharva8303 <118879041+atharva8303@users.noreply.github.com>
2025-03-17 14:46:13 +0530

    Time: 6 ms (10.81%), Space: 57.7 MB (51.24%) - LeetHub

--- 1787-sum-of-absolute-differences-in-a-sorted-array/1787-sum-of-absolute-differences-in-a-sorted-array.java
+++ 1787-sum-of-absolute-differences-in-a-sorted-array/1787-sum-of-absolute-differences-in-a-sorted-array.java
@@ -1,30 +1,31 @@
 class Solution {
     public int[] getSumAbsoluteDifferences(int[] nums) {
-        int l[]= new int[nums.length];
+        int prefix[]= new int[nums.length];
         int n = nums.length;
-        int r[]= new int[nums.length];
+        int suffix[]= new int[nums.length];
         int sum=0;int s=0;
         for (int i=0;i<n;i++)
         {
             sum=sum+nums[i];
-            l[i]=sum;
+            prefix[i]=sum;
+
             s=s+nums[n-i-1];
-            r[n-i-1]=s;
+            suffix[n-i-1]=s;
         }
         for(int i=0;i<n;i++)
         {
             if(i==0)
             {
-                nums[i]=r[i]-nums[i]*nums.length;
+                nums[i]=suffix[i]-nums[i]*nums.length;
             }
             else if(i==n-1)
             {
-                nums[i]=nums[i]*nums.length-l[i];
+                nums[i]=nums[i]*nums.length-prefix[i];
             }
             else{
 
                 
-                nums[i]=((nums[i]*i)-l[i-1])+(r[i+1]-nums[i]*(nums.length-i-1));
+                nums[i]=((nums[i]*i)-prefix[i-1])+(suffix[i+1]-nums[i]*(nums.length-i-1));
             }
         }
         return nums;
